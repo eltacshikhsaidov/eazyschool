a7e928a1277999e03aea73ff3b69c45fdab01530
eltacshikhsaidov <eltac.shixseyidov250301@gmail.com>
2022-03-16 22:51:43 +0400

    exception handling globally

--- src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
@@ -15,6 +15,7 @@ public class DashboardController {
     public String displayDashboard(Model model,Authentication authentication) {
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+        
         return "dashboard";
     }
 
